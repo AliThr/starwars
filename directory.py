a1d16e024a14a94934c42ad7a5367dd9ff8375dd
AliThr <admin@Alis-MacBook-Pro.local>
2018-01-29 13:54:48 +0000

    Added all planets with their numbers to the help page

--- directory.py
+++ directory.py
@@ -43,7 +43,107 @@ Planet Numbers:
 
 10: Kamino
 
-11: Geonosis"""
+11: Geonosis
+
+12. Utapu
+
+13. Mustafar
+
+14. Kashyyyk
+
+15. Polis Massa
+
+16. Mygeeto
+
+17. Felucia
+
+18. Cato Neimoidia
+
+19. Saleucami
+
+20. Stewjon
+
+21. Eriadu
+
+22. Corellia
+
+23. Rodia
+
+24. Nal Hutta
+
+25. Dantooine
+
+26. Bestine
+
+27. Ord Mantell
+
+28. Unknown
+
+29. Trandosha
+
+30. Socorro
+
+31. Mon Cala
+
+32. Chandrila
+
+33. Sullust
+
+34. Toydaria
+
+35. Malastare
+
+36. Dathomir
+
+37. Ryloth
+
+38. Aleen Minor
+
+39. Vulpter
+
+40. Troiken
+
+41. Tund
+
+42. Haruun Kal
+
+43. Cerea
+
+44. Glee Anselm
+
+45. Iridonia
+
+46. Tholoth
+
+47. Iktotch
+
+48. Quermia
+
+49. Dorin
+
+50. Champala
+
+51. Mirial
+
+52. Serenno
+
+53. Concord Dawn
+
+54. Zolan
+
+55. Ojom
+
+56. Skako
+
+57. Muunilinst
+
+58. Shili
+
+59. Kalee
+
+60. Umbara
+
+61. Jakku"""
 
 
 class APIGrabber():
